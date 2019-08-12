d046898630a178966fd167df4e4ffad4baf02c0b
systembug <albert_lee@hotmail.com>
2019-08-12 13:38:12 -0700

    3.0.3

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ nuget restore src/StatesButton.Forms.sln -verbosity detailed
 msbuild src/tatesButton.Forms.sln /p:Configuration=Release
 
 nuget pack nuspec/StatesButton.Forms.nuspec
-nuget add StatesButton.Forms.3.0.2.nupkg -source ~/localpackages
+nuget add StatesButton.Forms.3.0.3.nupkg -source ~/localpackages
